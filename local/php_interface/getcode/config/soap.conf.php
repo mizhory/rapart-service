5581e4b72519e97c7d2295990ce6cc43d5cea2f9
mizhory <mizhory@gmail.com>
2021-06-28 20:14:51 +0300

    Updated config files

--- local/php_interface/getcode/config/soap.conf.php
+++ local/php_interface/getcode/config/soap.conf.php
@@ -3,12 +3,9 @@ use GetCode\Manager\SoapConfigManager,
     GetCode\Helper\SoapConfigEntry;
 
 define('SOAP_CONFIG_INCLUDED', TRUE);
-define('SOAP_CONFIG_API_URI', 'https://erpdev.rapart.aero/rapart_ka_copy3/ws/crm_Integration?wsdl');
-//SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::URI));
-define('SOAP_CONFIG_API_LOGIN', 'User6');
-//SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::LOGIN));
-define('SOAP_CONFIG_API_PASS', 'PJJ3midtq');
-//SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::PASS));
+define('SOAP_CONFIG_API_URI', SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::URI));
+define('SOAP_CONFIG_API_LOGIN', SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::LOGIN));
+define('SOAP_CONFIG_API_PASS', SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::PASS));
 
 /*
  * $nUrl = 'https://erpdev.rapart.aero/rapart_ka_copy3/ws/crm_Integration?wsdl';
