e94881b2182721744520900b6f4c9d2233dc9edc
mizhory <mizhory@gmail.com>
2021-06-29 22:38:08 +0300

    Update 29.06.2021 2239

--- local/components/GetCode/order.list/templates/.default/template.php
+++ local/components/GetCode/order.list/templates/.default/template.php
@@ -25,14 +25,14 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
         ?>
         <tr>
             <td class="products__name"><?=$k?></td>
-            <td class="products__name"><?=date('d-m-Y H:i:s', intval($arItems['UF_TIMESTAMP']))?></td>
+            <td class="products__name"><?=date('d-m-Y|T+3|H:i:s')?></td>
             <td class="products__name"><?=$arItems['UF_USER_ID']?></td>
-            <td class="products__name"><?=date('d-m-Y H:i:s', intval($arItems['UF_TIMESTAMP']))?></td>
-            <td class="products__name">Сумма</td>
-            <td class="products__name">Состояние</td>
-            <td class="products__name">% оплаты</td>
-            <td class="products__name">% отгрузки</td>
-            <td class="products__name">Действия</td>
+            <td class="products__name"><?=date('d-m-Y|T+3|H:i:s')?></td>
+            <td class="products__name"><?=$arItems['PRICE']?></td>
+            <td class="products__name">Состояние<?#$arItems['UF_STATUS']?></td>
+            <td class="products__name">% оплаты<?#$arItems['UF_PERC_PRICE']?></td>
+            <td class="products__name">% отгрузки<?#$arItems['UF_PERC_OTG']?></td>
+            <td class="products__name"><a href="#order/?id=' . $line->ID . '&part=' . $curPage . '" class="product__btn">Посмотреть</a></td>
         </tr>
     <?endforeach;?>
 	<!--	<tr>
