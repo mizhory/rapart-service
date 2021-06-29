787d2c9cbe85a47f86869b6c362163531849913d
mizhory <mizhory@gmail.com>
2021-06-29 22:13:34 +0300

    Update 29.06.2021 2213

--- local/classes/GetCode/Manager/OffersManager.php
+++ local/classes/GetCode/Manager/OffersManager.php
@@ -13,13 +13,25 @@ class OffersManager
     public static function getElementsByIblock($inner = false) {
         if(is_bool($inner)) return null;
         static::init();
-        $arSelect = ['*', 'CATALOG_GROUP_ID_'.static::CATALOG_GROUP_ID];
+        $arSelect = ['*'];
         $arFilter = ['IBLOCK_ID' => static::IBLOCK_ID, 'ID' => $inner];
         $res = \CIBlockElement::GetList(Array(), $arFilter, false, Array("nPageSize"=>50), $arSelect);
         $ret = null;
         while($r=$res->GetNextElement()){
             $f = $r->GetFields();
             $f['PROPERTIES'] = $r->GetProperties();
+            $db_res = CPrice::GetList(
+                array(),
+                array(
+                    "PRODUCT_ID" => $r['ID'],
+                    "CATALOG_GROUP_ID" => static::CATALOG_GROUP_ID
+                )
+            );
+            if ($ar_res = $db_res->Fetch())
+            {
+                $f['PRICE'] = $ar_res;
+                $f['PRICE']['CURRENCY_VALUE'] = CurrencyFormat($ar_res["PRICE"], $ar_res["CURRENCY"]);
+            }
             $ret[$f['ID']] = $f;
         }
         return $ret;
