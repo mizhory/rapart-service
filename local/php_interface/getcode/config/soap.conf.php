60abaae9b5497de9ce6e4fa02b5f105e9a06d450
server-developer <server-developer@get-code.ru>
2021-07-05 19:18:27 +0300

    Merge update

--- local/php_interface/getcode/config/soap.conf.php
+++ local/php_interface/getcode/config/soap.conf.php
@@ -3,9 +3,9 @@ use GetCode\Manager\SoapConfigManager,
     GetCode\Helper\SoapConfigEntry;
 
 define('SOAP_CONFIG_INCLUDED', TRUE);
-define('SOAP_CONFIG_API_URI', SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::URI));
-define('SOAP_CONFIG_API_LOGIN', SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::LOGIN));
-define('SOAP_CONFIG_API_PASS', SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::PASS));
+define('SOAP_CONFIG_API_URI', 'https://erpdev.rapart.aero/rapart_ka_copy4/ws/crm_Integration?wsdl');//SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::URI));
+define('SOAP_CONFIG_API_LOGIN', 'DevRapart4');//SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::LOGIN));
+define('SOAP_CONFIG_API_PASS', '12345');//SoapConfigManager::getInstance()->getConfigSoap(SoapConfigEntry::PASS));
 
 /*
  * $nUrl = 'https://erpdev.rapart.aero/rapart_ka_copy3/ws/crm_Integration?wsdl';
