0d781ac7e81c1d5ae51b80a292dbc20258f54b84
deviloper <devil@oper.local>
2021-07-28 10:35:26 +0300

    Update 28072021 1035

--- local/components/GetCode/element.detail/templates/.default/invoice_tpl.php
+++ local/components/GetCode/element.detail/templates/.default/invoice_tpl.php
@@ -1,3 +1,3 @@
 <?if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)die;?>
-
+<h2>Счет детально - ##########</h2>
 <?var_dump($arResult);?>
--- local/components/GetCode/element.detail/templates/.default/kp_tpl.php
+++ local/components/GetCode/element.detail/templates/.default/kp_tpl.php
@@ -1,2 +1,3 @@
 <?if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)die;?>
+<h2>Клиентское предложение детально - ##########</h2>
 <?var_dump($arResult);?>
