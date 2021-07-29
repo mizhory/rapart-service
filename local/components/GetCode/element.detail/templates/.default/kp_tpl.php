7d1bd5105a5687254761f017c8b15198f90ec575
deviloper <devil@oper.local>
2021-07-29 22:25:02 +0300

    Update 29072021 2224

--- local/components/GetCode/element.detail/templates/.default/kp_tpl.php
+++ local/components/GetCode/element.detail/templates/.default/kp_tpl.php
@@ -1,3 +1,17 @@
 <?if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)die;?>
-<h2>Комерческое предложение <?=$arResult['UF_CO_ID']?> - детально</h2>
+<h2>Комерческое предложение <?=$arResult['ELEMENT']['UF_CO_ID']?> - детально</h2>
+<table class="products__block shop">
+    <thead>
+    <tr class="products__head">
+        <th class="products__name">№</th>
+        <th class="products__name">Дата</th>
+        <th class="products__name">Сумма</th>
+        <th class="products__name">Срок действия</th>
+        <th class="products__name">Состояние</th>
+        <th class="products__name">Действия</th>
+    </tr>
+    </thead>
+    <tbody>
 
+    </tbody>
+</table>
