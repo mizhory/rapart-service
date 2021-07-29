ff980ca21a786495bdc28ca401b1c739c4dbc6b0
deviloper <devil@oper.local>
2021-07-29 15:37:16 +0300

    Update 29072021 1537

--- local/classes/GetCode/Agent/SoapAgent.php
+++ local/classes/GetCode/Agent/SoapAgent.php
@@ -211,6 +211,7 @@ class SoapAgent {
                     }
                 }
             } elseif($method_step == StepingHelper::STEP_GET_KP) {
+                var_dump($data_step);
                 foreach($data_step as $user_xml_id=>$_user_data){
                     foreach($_user_data as $k=>$user_data) {
                         foreach($user_data["Tovary"] as $r=>$arItems) {
